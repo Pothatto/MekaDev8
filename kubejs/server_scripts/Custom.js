7f957a2360ab0a1e8524c910cf79fe59d61abda1
Pothatto <156546772+Pothatto@users.noreply.github.com>
2025-02-16 21:14:46 -0300

    MekaDev8 V8

--- kubejs/server_scripts/Custom.js
+++ kubejs/server_scripts/Custom.js
@@ -26,4 +26,45 @@ ServerEvents.recipes(event =>{
           "item": "mysticalagriculture:essence_vessel"
         }
       })
+event.custom({
+    "type": "mysticalagriculture:awakening",
+    "essences": [
+      {
+        "item": "mysticalagriculture:air_essence",
+        "count": 40
+      },
+      {
+        "item": "mysticalagriculture:earth_essence",
+        "count": 40
+      },
+      {
+        "item": "mysticalagriculture:water_essence",
+        "count": 40
+      },
+      {
+        "item": "mysticalagriculture:fire_essence",
+        "count": 40
+      }
+    ],
+    "input": {
+      "item": "mysticalexpansion:divinium_essence"
+    },
+    "ingredients": [
+      {
+        "item": 'mna:transitory_tunnel'
+      },
+      {
+        "item": 'draconicevolution:chaos_shard'
+      },
+      {
+        "item": 'botania:rune_pride'
+      },
+      {
+        "item": 'ae2:singularity'
+      }
+    ],
+    "result": {
+      "item": 'mysticalagradditions:creative_essence'
+    }
+}).id('mekaminers:create_creative_fase/creative_essence')
 })
